3065afa57f4cfdcd369a5718866a3ff6613e5511
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-15 15:40:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const byteSize = (str) => {
   // write your code here
+	let len=0
+	if(str.length===0){
+		return 0
+	}
+	for (let index = 0; index < str.length; index++) {
+		len+=Buffer.byteLength(str[index], "utf8")
+	}
+	return "length is "+len
 };
 
 // Do not change the code below
 const str = prompt("Enter some string.");
-alert(byteSize(str));
+alert(byteSize(str)); 
